cb69b56786cf91647b90f54897a639e5f6f0abc4
mohamed amine bahij <go4computergeek@gmail.com>
2018-05-18 14:34:51 +0000

    Added the UI for the register screen

--- app/src/main/java/com/v1rex/smartincubator/Activities/LoginActivity.java
+++ app/src/main/java/com/v1rex/smartincubator/Activities/LoginActivity.java
@@ -3,10 +3,17 @@ package com.v1rex.smartincubator.Activities;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.v1rex.smartincubator.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText mUserNameEditText;
+    EditText mPasswordEditText;
+    Button mLoginEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/v1rex/smartincubator/Activities/RegisterActivity.java
+++ app/src/main/java/com/v1rex/smartincubator/Activities/RegisterActivity.java
@@ -1,15 +1,26 @@
 package com.v1rex.smartincubator.Activities;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.v1rex.smartincubator.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText mUserNameEditText;
+    EditText mPasswordEditText;
+    EditText mRePasswordEditText;
+    Button mLoginEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
     }
 }
