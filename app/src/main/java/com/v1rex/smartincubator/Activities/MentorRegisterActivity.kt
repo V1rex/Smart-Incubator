6031d6c1a12db4b8cf5b680c64824d2e117ada6d
V1rex <go4computergeek@gmail.com>
2018-08-20 23:12:08 +0100

    deleted some boilerplate code from MentorRegisterActivity

--- app/src/main/java/com/v1rex/smartincubator/Activities/MentorRegisterActivity.kt
+++ app/src/main/java/com/v1rex/smartincubator/Activities/MentorRegisterActivity.kt
@@ -14,6 +14,8 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.v1rex.smartincubator.Model.Mentor
 import com.v1rex.smartincubator.R
+import kotlinx.android.synthetic.main.activity_mentor_register.*
+
 
 class MentorRegisterActivity : AppCompatActivity() {
 
@@ -21,90 +23,50 @@ class MentorRegisterActivity : AppCompatActivity() {
     private val ref = database.getReference("Data")
     private var mAuth: FirebaseAuth? = null
 
-    private var mLastNameEditText: EditText? = null
-    private var mFirstNameEditText: EditText? = null
-    private var mCityEditText: EditText? = null
-    private var mSpecialityEditText: EditText? = null
-    private var mEmailEditText: EditText? = null
-    private var mPhoneNumberEditText: EditText? = null
-    private var mLastNameTextInput: TextInputLayout? = null
-    private var mFirstNameTextInput: TextInputLayout? = null
-    private var mCityTextInput: TextInputLayout? = null
-    private var mSpecialityTextInput: TextInputLayout? = null
-    private var mEmailTextInput: TextInputLayout? = null
-    private var mPhoneNumberTextInput: TextInputLayout? = null
-    private var mSubmitMentorBtn: Button? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mentor_register)
+
         // get instance of the Auth firebase
         mAuth = FirebaseAuth.getInstance()
 
-        mLastNameEditText = findViewById<View>(R.id.last_name_edit_text_mentor) as EditText
-        mLastNameTextInput = findViewById<View>(R.id.last_name_input_text_mentor) as TextInputLayout
-
-
-        mFirstNameEditText = findViewById<View>(R.id.first_name_edit_text_mentor) as EditText
-        mFirstNameTextInput = findViewById<View>(R.id.first_name_input_text_mentor) as TextInputLayout
-
-        mCityEditText = findViewById<View>(R.id.city_edit_text_mentor) as EditText
-        mCityTextInput = findViewById<View>(R.id.city_input_text_mentor) as TextInputLayout
-
-        mSpecialityEditText = findViewById<View>(R.id.speciality_edit_text_mentor) as EditText
-        mSpecialityTextInput = findViewById<View>(R.id.speciality_input_text_mentor) as TextInputLayout
-
-        mEmailEditText = findViewById<View>(R.id.email_edit_text_mentor) as EditText
-        mEmailTextInput = findViewById<View>(R.id.email_input_text_mentor) as TextInputLayout
-
-        mPhoneNumberEditText = findViewById<View>(R.id.number_phone_edit_text_mentor) as EditText
-        mPhoneNumberTextInput = findViewById<View>(R.id.number_phone_input_text_mentor) as TextInputLayout
-
-
-        mSubmitMentorBtn = findViewById<View>(R.id.submit_action_btn_mentor) as Button
-        mSubmitMentorBtn!!.setOnClickListener { submit() }
-
-
-    }
-
-    override fun onBackPressed() {
-
+        submit_action_btn_mentor.setOnClickListener { submit() }
     }
 
     private fun submit() {
-        val lastName = mLastNameEditText!!.text.toString()
-        val firstName = mFirstNameEditText!!.text.toString()
-        val city = mCityEditText!!.text.toString()
-        val specialty = mSpecialityEditText!!.text.toString()
-        val email = mEmailEditText!!.text.toString()
-        val phoneNumber = mPhoneNumberEditText!!.text.toString()
+        val lastName = last_name_edit_text_mentor.text.toString()
+        val firstName = first_name_edit_text_mentor.text.toString()
+        val city = city_edit_text_mentor.text.toString()
+        val specialty = speciality_edit_text_mentor.text.toString()
+        val email = email_edit_text_mentor.text.toString()
+        val phoneNumber = number_phone_edit_text_mentor.text.toString()
 
         // boolean for cancelling the submit if something is wrong
         var cancel = false
 
         if (TextUtils.isEmpty(lastName)) {
-            mLastNameTextInput!!.error = getString(R.string.field_requierd)
+            last_name_input_text_mentor.error = getString(R.string.field_requierd)
             cancel = true
         }
         if (TextUtils.isEmpty(firstName)) {
-            mFirstNameTextInput!!.error = getString(R.string.field_requierd)
+            first_name_input_text_mentor.error = getString(R.string.field_requierd)
             cancel = true
         }
         if (TextUtils.isEmpty(city)) {
-            mCityTextInput!!.error = getString(R.string.field_requierd)
+            city_input_text_mentor.error = getString(R.string.field_requierd)
             cancel = true
         }
         if (TextUtils.isEmpty(specialty)) {
-            mSpecialityTextInput!!.error = getString(R.string.field_requierd)
+            speciality_input_text_mentor.error = getString(R.string.field_requierd)
             cancel = true
         }
         if (TextUtils.isEmpty(email)) {
-            mEmailTextInput!!.error = getString(R.string.field_requierd)
+            email_input_text_mentor.error = getString(R.string.field_requierd)
             cancel = true
 
         }
         if (TextUtils.isEmpty(phoneNumber)) {
-            mPhoneNumberTextInput!!.error = getString(R.string.field_requierd)
+            number_phone_input_text_mentor.error = getString(R.string.field_requierd)
             cancel = true
         }
 
@@ -117,10 +79,12 @@ class MentorRegisterActivity : AppCompatActivity() {
             val mentorRef = mentorsRef.child(mentor.mUserId)
             mentorRef.setValue(mentor)
 
-
             startActivity(Intent(this@MentorRegisterActivity, BottonNavigationActivity::class.java))
         }
 
+    }
+
+    override fun onBackPressed() {
 
     }
 }
