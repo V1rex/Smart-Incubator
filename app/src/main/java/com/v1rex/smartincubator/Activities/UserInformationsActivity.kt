cc034f1764df5863740d5828d4a81bfbdef5931a
V1rex <go4computergeek@gmail.com>
2018-08-21 03:55:06 +0100

    deleted some boilerplate code from UserInformationsActivity

--- app/src/main/java/com/v1rex/smartincubator/Activities/UserInformationsActivity.kt
+++ app/src/main/java/com/v1rex/smartincubator/Activities/UserInformationsActivity.kt
@@ -16,117 +16,73 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.v1rex.smartincubator.Model.User
 import com.v1rex.smartincubator.R
+import kotlinx.android.synthetic.main.activity_user_informations.*
 
 class UserInformationsActivity : AppCompatActivity() {
+
     private val database = FirebaseDatabase.getInstance()
     private val ref = database.getReference("Data")
     private var mAuth: FirebaseAuth? = null
 
-    private var mFirstNameTextInputLayout: TextInputLayout? = null
-    private var mLastNameTextInputLayout: TextInputLayout? = null
-    private var mEmailTextInputLayout: TextInputLayout? = null
-    private var mAgeTextInputLayout: TextInputLayout? = null
-    private var mFirstNameEditText: EditText? = null
-    private var mLastNameEditText: EditText? = null
-    private var mEmailEditText: EditText? = null
-    private var mAgeEditText: EditText? = null
-    private var mMaleButton: RadioButton? = null
-    private var mFemaleButton: RadioButton? = null
-    private var mStartupButton: RadioButton? = null
-    private var mMentorButton: RadioButton? = null
-    private var mSubmit: Button? = null
-    private var mSexeRequierdTextView: TextView? = null
-    private var mAccountTypeRequierdTextView: TextView? = null
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_informations)
+
         mAuth = FirebaseAuth.getInstance()
 
-        mFirstNameTextInputLayout = findViewById<View>(R.id.first_name_input_text) as TextInputLayout
-        mFirstNameEditText = findViewById<View>(R.id.first_name_edit_text) as EditText
-
-        mLastNameTextInputLayout = findViewById<View>(R.id.last_name_input_text) as TextInputLayout
-        mLastNameEditText = findViewById<View>(R.id.last_name_edit_text) as EditText
-
-        mEmailTextInputLayout = findViewById<View>(R.id.email_input_text) as TextInputLayout
-        mEmailEditText = findViewById<View>(R.id.email_edit_text) as EditText
-
-        mAgeTextInputLayout = findViewById<View>(R.id.age_input_text) as TextInputLayout
-        mAgeEditText = findViewById<View>(R.id.age_edit_text) as EditText
-
-        mMaleButton = findViewById<View>(R.id.male_selected) as RadioButton
-
-        mFemaleButton = findViewById<View>(R.id.female_selected) as RadioButton
-        mStartupButton = findViewById<View>(R.id.startup_button) as RadioButton
-
-        mMentorButton = findViewById<View>(R.id.mentor_button) as RadioButton
-
-        mSexeRequierdTextView = findViewById<View>(R.id.sexe_requierd_text_view) as TextView
-        mAccountTypeRequierdTextView = findViewById<View>(R.id.account_type_requierd_text_view) as TextView
-
-        mSubmit = findViewById<View>(R.id.submit_action_btn) as Button
-
         // get the email of the user
-        mEmailEditText!!.setText(mAuth!!.currentUser!!.email)
-
-        mSubmit!!.setOnClickListener { submit() }
-
-    }
-
-    override fun onBackPressed() {
-
+        email_edit_text.setText(mAuth!!.currentUser!!.email)
+        submit_action_btn.setOnClickListener { submit() }
     }
 
     /**
      * Method for submitting the user informations
      */
     private fun submit() {
-        mSexeRequierdTextView!!.visibility = View.GONE
-        mAccountTypeRequierdTextView!!.visibility = View.GONE
+        sexe_requierd_text_view.visibility = View.GONE
+        account_type_requierd_text_view.visibility = View.GONE
         // a boolean to cancel submit if something is not good
         var cancel = false
 
-        val lastName = mLastNameEditText!!.text.toString()
-        val firstName = mFirstNameEditText!!.text.toString()
-        val email = mEmailEditText!!.text.toString()
-        val age = mAgeEditText!!.text.toString()
+        val lastName = last_name_edit_text.text.toString()
+        val firstName = first_name_edit_text.text.toString()
+        val email = email_edit_text.text.toString()
+        val age = age_edit_text.text.toString()
         var sexe = ""
         var accountType = ""
 
 
-        val maleSelected = mMaleButton!!.isChecked
-        val femaleSelected = mFemaleButton!!.isChecked
-        val startupSelected = mStartupButton!!.isChecked
-        val mentorSlected = mMentorButton!!.isChecked
+        val maleSelected = male_selected.isChecked
+        val femaleSelected = female_selected.isChecked
+        val startupSelected = startup_button.isChecked
+        val mentorSlected = mentor_button.isChecked
 
         if (!isEmailValid(email)) {
-            mEmailTextInputLayout!!.error = getString(R.string.error_invalid_email)
+            email_input_text.error = getString(R.string.error_invalid_email)
             cancel = true
         } else if (TextUtils.isEmpty(email)) {
-            mEmailTextInputLayout!!.error = getString(R.string.field_requierd)
+            email_input_text.error = getString(R.string.field_requierd)
             cancel = true
         }
 
         if (TextUtils.isEmpty(lastName)) {
-            mLastNameTextInputLayout!!.error = getString(R.string.field_requierd)
+            last_name_input_text.error = getString(R.string.field_requierd)
             cancel = true
         }
 
         if (TextUtils.isEmpty(firstName)) {
-            mFirstNameTextInputLayout!!.error = getString(R.string.field_requierd)
+            first_name_input_text.error = getString(R.string.field_requierd)
             cancel = true
         }
 
         if (TextUtils.isEmpty(age)) {
-            mAgeTextInputLayout!!.error = getString(R.string.field_requierd)
+            age_input_text.error = getString(R.string.field_requierd)
             cancel = true
         }
 
         if (maleSelected == false && femaleSelected == false) {
             cancel = true
-            mSexeRequierdTextView!!.visibility = View.VISIBLE
+            sexe_requierd_text_view.visibility = View.VISIBLE
 
         } else {
             if (maleSelected == true) {
@@ -140,7 +96,7 @@ class UserInformationsActivity : AppCompatActivity() {
 
         if (startupSelected == false && mentorSlected == false) {
             cancel = true
-            mAccountTypeRequierdTextView!!.visibility = View.VISIBLE
+            account_type_requierd_text_view.visibility = View.VISIBLE
         } else {
             if (startupSelected == true) {
                 accountType = "Startup"
@@ -175,6 +131,10 @@ class UserInformationsActivity : AppCompatActivity() {
     private fun isEmailValid(email: String): Boolean {
         //TODO: Replace this with your own logic
         return email.contains("@")
+    }
+
+    override fun onBackPressed() {
+
     }
 
 }
