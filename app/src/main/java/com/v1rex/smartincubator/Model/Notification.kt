a9fec83cc6741a4d612a8c5f61e6c580082955ca
V1rex <go4computergeek@gmail.com>
2018-08-29 00:47:48 +0100

    updated Notification object for firebase

--- app/src/main/java/com/v1rex/smartincubator/Model/Notification.kt
+++ app/src/main/java/com/v1rex/smartincubator/Model/Notification.kt
@@ -1,4 +1,4 @@
 package com.v1rex.smartincubator.Model
 
-class Notification (var senderUid : String = "" , var receiverUid : String = "", var receiverToken :String = "", var messageBody : String = "" , var senderName : String = "" ) {
+class Notification (var senderUid : String = "" , var receiverUid : String = "", var receiverToken :String = "", var messageBody : String = "" , var senderName : String = "" , var specialty_needUser : String = "", var userType : String = "" ) {
 }
---
+++ app/src/main/java/com/v1rex/smartincubator/Services/MyFirebaseMessagingService.kt
@@ -0,0 +1,4 @@
+package com.v1rex.smartincubator.Services
+
+class MyFirebaseMessagingService {
+}
