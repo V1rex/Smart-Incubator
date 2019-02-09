da7044a9239a5b2e662e8d5b45ddb7781ce7b16d
V1rex <go4computergeek@gmail.com>
2019-02-09 23:32:20 +0100

    Reduced apk size

--- app/src/main/java/com/v1rex/smartincubator/ViewHolder/MessagesViewHolder.kt
+++ app/src/main/java/com/v1rex/smartincubator/ViewHolder/MessagesViewHolder.kt
@@ -4,7 +4,6 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
-import com.google.android.gms.flags.impl.DataUtils
 import com.v1rex.smartincubator.R
 import java.text.SimpleDateFormat
 import java.util.*
