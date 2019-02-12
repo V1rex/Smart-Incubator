5132d7efb8a50c1e3f68c93ce2f2310284dc8eba
V1rex <go4computergeek@gmail.com>
2019-02-12 06:49:22 +0100

    Change Startup Class code

--- app/src/main/java/com/v1rex/smartincubator/Model/Startup.kt
+++ app/src/main/java/com/v1rex/smartincubator/Model/Startup.kt
@@ -1,6 +1,40 @@
 package com.v1rex.smartincubator.Model
 
+class Startup{
+
+    var mStartupName: String = ""
+    var mAssociate: String = ""
+    var mDescription: String = ""
+    var  mWebsite: String = ""
+    var mPageFacebook: String = ""
+    var mDateOfIncubation: String = ""
+    var mJuridiqueSatatus: String = ""
+    var  mCreationDate: String = ""
+    var  mNumberEmployees: String = ""
+    var mObjective: String = ""
+    var  mFond: String = ""
+    var mChiffre: String = ""
+    var mUserId: String = ""
+    var mNeed: String = ""
+    var  mDomain: String = ""
+
+    constructor (startupName: String , associate: String , description: String, website: String , pageFacebook: String , dateOfIncubation: String , juridiqueSatatus: String , creationDate: String ,numberEmployees: String , objective: String, fond: String , chiffre: String , userId: String , need: String , domain: String ){
+        mStartupName = startupName
+        mAssociate = associate
+        mDescription = description
+        mWebsite = website
+        mPageFacebook = pageFacebook
+        mDateOfIncubation = dateOfIncubation
+        mJuridiqueSatatus = juridiqueSatatus
+        mCreationDate = creationDate
+        mNumberEmployees = numberEmployees
+        mObjective = objective
+        mFond = fond
+        mChiffre = chiffre
+        mUserId = userId
+        mNeed = need
+        mDomain = domain
 
 
-class Startup constructor (var mStartupName: String = "", var mAssociate: String = "", var mDescription: String = "", var  mWebsite: String = "", var mPageFacebook: String = "", var mDateOfIncubation: String = "", var mJuridiqueSatatus: String = "", var  mCreationDate: String = "", var  mNumberEmployees: String = "", var mObjective: String = "", var  mFond: String = "", var mChiffre: String = "", var mUserId: String = "", var mNeed: String = "", var  mDomain: String = "") {
+    }
 }
