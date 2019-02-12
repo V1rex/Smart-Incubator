5658cbe159e64a8d9a1268d54f9ce4492a7e6414
V1rex <go4computergeek@gmail.com>
2019-02-12 06:35:14 +0100

    Changed Mentor code

--- app/src/main/java/com/v1rex/smartincubator/Model/Mentor.kt
+++ app/src/main/java/com/v1rex/smartincubator/Model/Mentor.kt
@@ -2,6 +2,24 @@ package com.v1rex.smartincubator.Model
 
 import java.io.Serializable
 
-class Mentor constructor(var mCity: String = "" ,var mSpeciality: String = "" ,var mLastName: String = "", var mFirstName: String = "",var  mEmail: String = "", var mPhoneNumber: String = "", var mUserId: String = "") {
+class Mentor  {
+    var mCity : String = ""
+    var mSpeciality : String = ""
+    var mLastName : String = ""
+    var mFirstName : String = ""
+    var mEmail : String = ""
+    var mPhoneNumber : String = ""
+    var mUserId : String = ""
+
+    constructor(city: String , speciality: String ,lastName: String, firstName: String ,email: String , phoneNumber: String , userId: String = ""){
+        mCity = city
+        mSpeciality = speciality
+        mLastName = lastName
+        mFirstName = firstName
+        mEmail = email
+        mPhoneNumber = phoneNumber
+        mUserId = userId
+    }
+
 
 }
